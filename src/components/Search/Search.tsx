fe3265d54cb5cc366076877f4486101c6d6cb5f6
Lawson Hung <lawsonhung@yahoo.com>
2026-02-27 18:05:29 -0500

    Set up suggestions state

--- src/components/Search/Search.tsx
+++ src/components/Search/Search.tsx
@@ -1,14 +1,32 @@
 import { Autocomplete, TextField, type AutocompleteRenderInputParams } from "@mui/material";
+import { useEffect, useState, type ChangeEvent } from "react";
 
 const Search = () => {
+
+  useEffect(() => {
+    
+  }, [])
+
+  const [suggestions, setSuggestions] = useState([]);
+
+  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+    console.log(e.target.value)
+  }
 
   return (
     <>
       <Autocomplete
         renderInput={(params: AutocompleteRenderInputParams): React.ReactNode => {
-          return <TextField {...params} label="Search" variant="standard" />
+          return <TextField
+          {...params}
+          name="suggestions"
+          label="Search"
+          variant="standard"
+          value={suggestions}
+          onChange={handleChange}
+          />
         }}
-        options={["Test"]}
+        options={suggestions}
         sx={{ width: 300 }}
       />
     </>
