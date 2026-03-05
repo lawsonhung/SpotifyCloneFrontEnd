93bb7abf64ea42bacc981dc424cfdb227127f3a7
Lawson Hung <lawsonhung@yahoo.com>
2026-03-05 01:07:12 -0500

    Display artist background image

--- src/pages/Results/Results.tsx
+++ src/pages/Results/Results.tsx
@@ -1,6 +1,8 @@
 import { useSelector } from "react-redux";
-import "./Results.css";
 import type { RootState } from "../../app/store";
+import { Box, Typography } from "@mui/material";
+import Image from "mui-image";
+import type { Artist } from "@spotify/web-api-ts-sdk";
 
 const Results = () => {
 
@@ -8,10 +10,35 @@ const Results = () => {
 
   console.log("mainDisplayItem", mainDisplayItem);
 
+  let backgroundImageUrl;
+  if ((mainDisplayItem.value as Artist).images)
+    backgroundImageUrl = (mainDisplayItem.value as Artist).images[0].url;
+
   return (
-    <div className="results">
-    <h1>Results</h1>
-    </div>
+    <Box
+      sx={{
+        height: "100%",
+        background: "linear-gradient(#2F3060, #181818)",
+        borderRadius: "8px"
+      }}
+    >
+      <Box
+        sx={{
+          height: "40%",
+          backgroundImage: `url(${backgroundImageUrl})`,
+          backgroundSize: "cover",
+          backgroundRepeat: "no-repeat",
+          backgroundPosition: "center",
+          display: "flex",
+          flexDirection: "column-reverse",
+        }}
+      >
+        <Typography variant="h1" fontWeight={900} margin={"0.1em"}>{(mainDisplayItem.value as Artist).name}</Typography>
+      </Box >
+      <div className="results">
+        <h1>Results</h1>
+      </div>
+    </Box >
   )
 }
 
