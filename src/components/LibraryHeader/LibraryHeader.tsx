a1a36694fdd4c9154b46683db0fe36166eccb422
Lawson Hung <lawsonhung@yahoo.com>
2026-03-10 13:02:12 -0400

    Library add playlist icon

--- src/components/LibraryHeader/LibraryHeader.tsx
+++ src/components/LibraryHeader/LibraryHeader.tsx
@@ -1,9 +1,11 @@
-import { IconButton, Stack, Typography } from "@mui/material";
+import { Box, Icon, IconButton, Stack, Typography } from "@mui/material";
 import { createPlaylist } from "../../api";
 import { useDispatch, useSelector } from "react-redux";
 import { setPlaylists } from "../../features/library/library";
 import type { RootState } from "../../app/store";
 import { setSnackbarMessage, setSnackbarOpen } from "../../features/snackbar/snackbar";
+import addCircle from "../../assets/addCircle.svg";
+import addCircleFilled from "../../assets/addCircleFilled.svg";
 
 const LibraryHeader = () => {
 
@@ -20,20 +22,39 @@ const LibraryHeader = () => {
 
   return (
     <Stack direction="row" justifyContent="space-between">
-        <Typography
-          fontWeight="bold"
-          paddingTop="0.5em"
-          paddingLeft="0.5em"
+      <Typography
+        fontWeight="bold"
+        paddingTop="0.5em"
+        paddingLeft="0.5em"
+      >
+        Your Library
+      </Typography>
+      <IconButton
+        aria-label="add new playlist"
+        onClick={handleClick}
+        sx={{
+          "&:hover": {
+            backgroundColor: "transparent",
+            "& .addIcon": {
+              display: "none",
+            },
+            "& .addIconHover": {
+              display: "block",
+            },
+          },
+          "& .addIconHover": {
+            display: "none",
+          }
+        }}
         >
-          Your Library
-        </Typography>
-        <IconButton
-          aria-label="add new playlist"
-          onClick={handleClick}
-        >
-          +
-        </IconButton>
-      </Stack>
+      <Icon className="addIcon">
+        <Box component={"img"} src={addCircle} alt="Add playlist" />
+      </Icon>
+      <Icon className="addIconHover">
+        <Box component={"img"} src={addCircleFilled} alt="Add playlist hover" />
+      </Icon>
+    </IconButton>
+      </Stack >
   )
 }
 
--- src/components/MainDisplayTrackItem/MainDisplayTrackItem.tsx
+++ src/components/MainDisplayTrackItem/MainDisplayTrackItem.tsx
@@ -35,8 +35,8 @@ const MainDisplayTrackItem = ({ track, index }: MainDisplayTrackItemProps) => {
 
         controllerRef.current = new AbortController();
         try {
-          const trackInfo = await getTrack(track.id, controllerRef.current.signal);
-          imgUrl.current = trackInfo.album.images[1].url;
+          // const trackInfo = await getTrack(track.id, controllerRef.current.signal);
+          // imgUrl.current = trackInfo.album.images[1].url;
         } catch (error) {
           if (axios.isCancel(error))
             console.log("Request canceled", error.message);
